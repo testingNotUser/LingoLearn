20a4b91fe12306ba1f2edfd8a31d6845775b7cde
testingNotUser <jhn.snw.0112@gmail.com>
2026-04-16 23:51:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Courses from './components/Courses';
+import Activities from './components/Activities';
+import Guides from './components/Guides';
 import Footer from './components/Footer';
-import LandingPage from './pages/LandingPage';
 import CoursesPage from './pages/CoursesPage';
 import QuizPage from './pages/QuizPage';
-import { useState, useEffect } from 'react';
 
 function App() {
-  const [user, setUser] = useState(localStorage.getItem('lingoUser'));
-
-  useEffect(() => {
-    const handleStorage = () => setUser(localStorage.getItem('lingoUser'));
-    window.addEventListener('storage', handleStorage);
-    return () => window.removeEventListener('storage', handleStorage);
-  }, []);
-
   return (
-    <Router>
-      <Navbar user={user} setUser={setUser} />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/courses" element={<CoursesPage />} />
-        <Route path="/quiz" element={<QuizPage />} />
-      </Routes>
-      <Footer />
+    <Router basename="/LingoLearn">
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <div id="courses">
+                <Courses />
+              </div>
+              <Activities />
+              <div id="about">
+                <Guides />
+              </div>
+            </>
+          } />
+          <Route path="/courses" element={<CoursesPage />} />
+          <Route path="/quiz" element={<QuizPage />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
